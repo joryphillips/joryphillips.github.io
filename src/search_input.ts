0adbaa5fd56340946e7261ca59c1ec7b75a35a78
Jory Phillips <jorylphillips@gmail.com>
2021-09-23 16:58:28 -0700

    implement ARIA 1.1 combobox 💪

--- src/search_input.ts
+++ src/search_input.ts
@@ -36,18 +36,31 @@ const styles = html`
         border-radius: 3px;
       }
 
-      div[role="listbox"] {
+      input[type="search"]:focus {
+        outline: 2px solid hsl(211deg 100% 40% / 90%)!important;
+      }
+
+      ul,
+      ul li,
+      ul ul li {
+        margin:0;
+        padding: 0;
+        text-indent: 0;
+        list-style-type: none;
+      }
+
+      ul[role="listbox"] {
         display: none;
         background-color: #FFF;
         border: 1px solid #ccc;
       }
 
-      div[role="listbox"], button[role="option"] {
+      ul[role="listbox"], button[role="option"] {
         box-sizing: border-box;
         width: 100%;
       }
 
-      div[role="listbox"].show {
+      ul[role="listbox"].show {
         display: block;
         position: absolute;
         z-index: 10;
@@ -55,7 +68,7 @@ const styles = html`
         overflow-y: scroll;
       }
 
-      button[role="option"] {
+      li[role="option"] {
         background-color: #FFF;;
         border: none;
         border-bottom: 1px solid #ccc;
@@ -64,7 +77,11 @@ const styles = html`
         padding: .5rem;
       }
 
-      button[role="option"]:last-of-type {
+      li[role="option"][active], li[role="option"]:hover {
+        background-color: rgb(115 179 221 / 17%);
+      }
+
+      li:last-of-type  button[role="option"] {
         border-bottom: none;
       }
     </style>
@@ -73,21 +90,71 @@ const styles = html`
 function SearchBox({keyWords, handleSearchInput}: SearchBoxProps) {
   const [showDropDown, setShowDropDown] = useState(false);
   const [inputDebounce, setInputDebounce] = useState(null);
+  const [optionIndex, setOptionIndex] = useState(null);
 
-  const searchInput = this.shadowRoot.querySelector(Selector.SEARCH_INPUT) as HTMLInputElement;
+  const searchInput: HTMLInputElement = this.shadowRoot.querySelector(Selector.SEARCH_INPUT);
+  const listBox: HTMLElement = this.shadowRoot.querySelector('#listbox');
 
   const toggleDropdown = () => {
     setShowDropDown(!showDropDown);
   };
 
   const handleFocus = () => {
-    toggleDropdown();
+    setShowDropDown(true);
   };
 
-  const handleBlur = (e: FocusEvent) => {
-    e.preventDefault();
+  const handleArrowDown = () => {
+    setShowDropDown(true);
+    let localIndex;
+    if (optionIndex == null || optionIndex === keyWords.size - 1) {
+      localIndex = 0;
+    } else {
+      localIndex = optionIndex + 1;
+    }
+    listBox.children[localIndex].scrollIntoView({ behavior: 'smooth', block: 'nearest' });
+    setOptionIndex(localIndex);
+  };
+
+  const handleArrowUp = () => {
+    let localIndex;
+    if (optionIndex == null || optionIndex === 0) {
+      localIndex = keyWords.size - 1;
+    } else {
+      localIndex = optionIndex - 1;
+    }
+    listBox.children[localIndex].scrollIntoView({ behavior: 'smooth', block: 'nearest' });
+    setOptionIndex(localIndex);
+  };
+
+  const handleInputKeyUp = (e: KeyboardEvent) => {
+    const {key} = e;
+    switch (key) {
+    case 'ArrowDown':
+      handleArrowDown();
+      break;
+    case 'ArrowUp':
+      handleArrowUp();
+      break;
+    case 'Escape':
+      handleSearchInput('');
+      setOptionIndex(null);
+      setShowDropDown(false);
+      break;
+    case 'Enter':
+      searchInput.value = [...keyWords][optionIndex];
+      handleSearchInput([...keyWords][optionIndex]);
+      setShowDropDown(false);
+      break;
+
+    default:
+      break;
+    }
+  };
+
+  const handleBlur = () => {
+    setOptionIndex(null);
     if (showDropDown) {
-      toggleDropdown();
+      setShowDropDown(false);
     }
   };
 
@@ -102,7 +169,7 @@ function SearchBox({keyWords, handleSearchInput}: SearchBoxProps) {
     }, DEBOUNCE_TIMEOUT));
   };
 
-  const onDropdownButtonClick = (keyword: string)=> {
+  const onOptionClick = (keyword: string)=> {
     searchInput.value = keyword;
 
     toggleDropdown();
@@ -111,24 +178,39 @@ function SearchBox({keyWords, handleSearchInput}: SearchBoxProps) {
 
   return html`
     ${styles}
-
+    <div
+      id="combobox"
+      role="combobox"
+      aria-expanded=${showDropDown}
+      aria-owns="listbox"
+      aria-haspopup="true">
     <label for="search" class="hide">Search</label>
-    <input
-      type="search"
-      placeholder="search"
-      aria-label="Search through projects"
-      autocomplete="off"
-      @input=${handleInput}
-      @focus=${handleFocus}
-      @blur=${handleBlur}
-    >
-    <div role="listbox" class=${classMap({'show': showDropDown})}>
-      ${[...keyWords].map(keyword => html`
-        <button
-          role="option"
-          @mousedown=${()=> onDropdownButtonClick(keyword)}>${keyword}</button>
-      `)}
+      <input
+        type="search"
+        role="combobox"
+        placeholder="search"
+        aria-label="Search through projects"
+        aria-controls="listbox"
+        autocomplete="off"
+        @input=${handleInput}
+        @focus=${handleFocus}
+        @blur=${handleBlur}
+        @keyup=${handleInputKeyUp}
+      >
     </div>
+
+    <ul
+      id="listbox"
+      role="listbox"
+      aria-expanded=${showDropDown}
+      class=${classMap({'show': showDropDown})}
+    >${[...keyWords].map((keyword: string, index: number) => html`
+        <li
+          role="option"
+          ?active=${index === optionIndex}
+          @mousedown=${()=> onOptionClick(keyword)}>${keyword}</li>
+      `)}
+    </ul>
   `;
 }
 
