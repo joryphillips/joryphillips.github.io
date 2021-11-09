159b6618c9d12da2a76819522733470be6b77d98
Jory Phillips <jorylphillips@gmail.com>
2021-11-09 15:30:05 -0800

    fix link info icon focus padding

--- src/components/project_card/styles.ts
+++ src/components/project_card/styles.ts
@@ -19,7 +19,11 @@ export const styles = html`
       padding: 2rem 0.5rem 1rem .5rem;
     }
 
-    .info-icons, .info-icons .info, .info-icons .link {
+    .info-icons {
+      margin-left: 0.7rem;
+    }
+
+    .info-icons, .info-icons .link {
       display: flex;
       align-items: center;
     }
@@ -27,7 +31,6 @@ export const styles = html`
     .info-icons .link {
       background-color: transparent;
       opacity: .7;
-      padding: 0 0 0 0.7rem;
     }
 
     .info-icons .link:hover {
