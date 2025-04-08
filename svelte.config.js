ca8bd3a261301a66a1beb19c154ad5e8677890a6
Jory Phillips <jory.l.phillips@gmail.com>
2025-04-07 19:05:41 -0700

    chore: try some config fixes

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,7 @@ const config = {
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			fallback: undefined,
+			fallback: 'index.html',
 			precompress: false,
 			strict: true,
 			fallback: '404.html'
